edb0810dfa0071f0ab254cffd405cf02de97df1a
Eder <eder.israel333@gmail.com>
2020-06-08 04:37:52 -0500

    nueva actualización

--- app/src/main/java/com/example/kokis/calcular.java
+++ app/src/main/java/com/example/kokis/calcular.java
@@ -4,8 +4,6 @@ package com.example.kokis;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
-import android.net.Uri;
-import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View.OnClickListener;
@@ -16,16 +14,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import java.util.ArrayList;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class calcular extends Activity implements OnClickListener {
 
     Button refresco, reyeno, ingrediente, ind, chica, mediana, grande, familiar, limpiar, cuadrada, eliminar, cal, reb;
     private Dialog dialog;
     TextView resultado;
-    EditText contenedor, cont;
+    EditText  cont, contenedor;
 
     ArrayList<Integer> lista = new ArrayList();
+    ArrayList<String> nombre = new ArrayList();
+
+    int cambiar = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,47 +74,64 @@ public class MainActivity extends Activity implements OnClickListener {
         reb = (Button) findViewById(R.id.reb);
         reb.setOnClickListener(this);
 
-        contenedor = (EditText) findViewById(R.id.contenedor);
-
         cont = (EditText) findViewById(R.id.conte);
 
         resultado = (TextView) findViewById(R.id.resultado);
 
+        contenedor = (EditText) findViewById(R.id.contenedor);
+        contenedor.setOnClickListener(this);
+
+        contenedor.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(getApplicationContext(), "Listo para modificar", Toast.LENGTH_SHORT).show();
+                modificar();
+                return false;
+            }
+        });
 
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.ingrediente) {
-            array(7);
+            array(7,"Extra");
         }
         if (v.getId() == R.id.refresco) {
-            array(10);
+
+            array(10, "Refresco");
         }
         if (v.getId() == R.id.reyena) {
-            int relleno = lista.size() - 1;
-            if (relleno >= 0) {
-                int pizza = lista.get(relleno);
-                relleno(pizza, relleno);
+
+            int numerolista = lista.size() - 1;
+            if (numerolista >= 0) {
+                String pizza = nombre.get(numerolista);
+                relleno(pizza, numerolista);
             }
         }
         if (v.getId() == R.id.individual) {
-            array(40);
+            array(40,"Individual");
+
         }
         if (v.getId() == R.id.chica) {
-            array(70);
+            array(70, "Chica");
+
+
         }
         if (v.getId() == R.id.mediana) {
-            array(100);
+            array(100,"Mediana");
+
         }
         if (v.getId() == R.id.grande) {
-            array(130);
+            array(130,"Grande");
+
         }
         if (v.getId() == R.id.familiar) {
-            array(155);
+            array(160,"Familiar");
+
         }
         if (v.getId() == R.id.cuadrada) {
-            array(220);
+            array(230, "Cuadrada");
         }
         if (v.getId() == R.id.cal) {
             calculadora();
@@ -124,51 +143,88 @@ public class MainActivity extends Activity implements OnClickListener {
             int dato = lista.size() - 1;
             if (dato >= 0) {
                 lista.remove(dato);
+                nombre.remove(dato);
             }
             suma();
+            contenedor.setSelection(contenedor.getText().length());
         }
         if (v.getId() == R.id.limpiar) {
+
             resultado.setText("");
             contenedor.setText("");
             lista.clear();
+            nombre.clear();
+        }
+        if (v.getId() == R.id.contenedor) {
+            if(cambiar==1){
+                cambiar=0;
+                contenedor.setText("" + lista);
+            }else{
+                cambiar=1;
+                contenedor.setText("" + nombre);
+            }
+            contenedor.setSelection(contenedor.getText().length());
         }
     }
 
-    public void array(int a) {
+
+
+    public void array(int a, String b) {
+        nombre.add(b);
         lista.add(a);
         suma();
+        contenedor.setSelection(contenedor.getText().length());
     }
 
     public void suma() {
+        if(cambiar==0){
         contenedor.setText("" + lista);
+        }
+        else{
+            contenedor.setText("" + nombre);
+        }
+
         int suma = 0;
         for (Integer i : lista) {
             suma = suma + i;
         }
+
         resultado.setText("" + suma);
+
     }
 
-    public void relleno(int b, int c) {
-        if (b == 40)
+    public void relleno(String b, int c) {
+        if (b.equals("Individual")) {
             lista.set(c, 50);
+            nombre.set(c, "Individual R");
+        }
 
-        if (b == 70)
+        if (b.equals("Chica")) {
             lista.set(c, 85);
+            nombre.set(c, "Chica R");
+        }
 
-        if (b == 100)
+        if (b == "Mediana") {
             lista.set(c, 120);
+            nombre.set(c, "Mediana R");
+        }
 
-        if (b == 130)
+        if (b == "Grande") {
             lista.set(c, 155);
+            nombre.set(c, "Grande R");
+        }
 
-        if (b == 155)
-            lista.set(c, 185);
+        if (b == "Familiar") {
+            lista.set(c, 195);
+            nombre.set(c, "Familiar R");
+        }
 
-        if (b == 220)
-            lista.set(c, 255);
-
-
+        if (b == "Cuadrada") {
+            lista.set(c, 280);
+            nombre.set(c, "Cuadrada R");
+        }
         suma();
+        contenedor.setSelection(contenedor.getText().length());
     }
 
     private void rebanadas() {
@@ -180,11 +236,19 @@ public class MainActivity extends Activity implements OnClickListener {
         dialog.findViewById(R.id.btCancelar).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (caja.getText().toString().isEmpty()) { }
+                if (caja.getText().toString().isEmpty() ){ }
                 else {
-                    String reb = caja.getText().toString();
-                    int reba = Integer.parseInt(reb) * 20;
-                    array(reba);
+                    if (caja.getText().length() > 9){
+                        Toast.makeText(getApplicationContext(), "Datos no soportados", Toast.LENGTH_SHORT).show();
+                    }
+                      else {
+                        String reb = caja.getText().toString();
+                        int reba = Integer.parseInt(reb) * 20;
+                        if (reba != 0) {
+                            String rebanadas = reb + " Reb";
+                            array(reba, rebanadas);
+                        }
+                    }
                 }
                 dialog.dismiss();
             }
@@ -272,6 +336,20 @@ public class MainActivity extends Activity implements OnClickListener {
 
             }
         });
+        dialog.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               if (caja.getText().toString().isEmpty() == false){
+                    String reb = caja.getText().toString();
+                    reb = reb.substring(0, reb.length() - 1);
+                    caja.setText("" + reb);
+                }
+
+            }
+        });
+
+
 
     }
 
@@ -373,18 +451,47 @@ public class MainActivity extends Activity implements OnClickListener {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+    public void modificar(){
+
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.modificar);
+
+
+
+       // final ListView modifica = (ListView) dialog.findViewById(R.id.modificar);
+        //ArrayAdapter adapta= new ArrayAdapter(this,android.R.layout.simple_list_item_1,nombre);
+        //modifica.setAdapter(adapta);
+
+
+
+
+
+
+
+        dialog.findViewById(R.id.btCancelar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.setCancelable(false);
+        dialog.show();
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.modificar) {
-            Intent intent = new Intent(this, MainActivity.class);
+        if (id == R.id.promo) {
+            Intent intent = new Intent(this, calcular.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
